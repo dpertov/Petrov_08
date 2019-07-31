a3df7e394024b63bba5e1d9d9ddee2bb104df388
dpertov <52155563+dpertov@users.noreply.github.com>
2019-07-31 16:37:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var padstring = function addsymbol (name, resultLength, sideRight) {
+var padstring = function addsymbol (name, resultLength, sideRight,symbol) {
 	sideRight = sideRight || false;
 	resultLength = resultLength || 10;
-	
+	symbol = symbol || '*';
 	if (name.length >= resultLength) {
 		return name;
 	}
 	else {
-		var o = '*';
-		sideRight ? name =  name + o : name = o + name;
+		
+		sideRight ? name =  name + symbol : name = symbol + name;
 		console.log(name);
-		return addsymbol (name,resultLength,sideRight);
+		return addsymbol (name,resultLength,sideRight,symbol);
 	}
 }
 	
 var userName = prompt('Введите имя');
 var len = +prompt ('Введите общее количество символов');
-var isRight = confirm ('Подставляем звездочки после имени?');
-alert('результат = ' + padstring(userName,len,isRight));
+var symbol = prompt ('Введите символ для подставления в имя');
+var isRight = confirm ('Подставляем символ после имени?');
+alert('результат = ' + padstring(userName,len,isRight, symbol));
 
